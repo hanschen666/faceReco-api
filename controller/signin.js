394bfd4195ffb6b7a2c79ea2cda55e318150d119
hanschen666 <15814841133@163.com>
2021-12-27 02:20:02 +0800

    First commit

---
+++ controller/signin.js
@@ -0,0 +1,26 @@
+const handleSignin = (req, res, db, bcrypt) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isCorrect = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isCorrect === true) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to find user"));
+      } else {
+        res.status(400).json("Wrong credentials!");
+      }
+    })
+    .catch((err) => res.status(400).json("Wrong credentials!"));
+};
+
+export const signin = handleSignin;
---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import knex from "knex";
+import bcrypt from "bcrypt";
+import { register } from "./controller/register.js";
+import { signin } from "./controller/signin.js";
+import { profile } from "./controller/profile.js";
+import { image, apiCall } from "./controller/image.js";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "superfish",
+    password: "",
+    database: "intelligent-face-recognition",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/signin", (req, res) => {
+  signin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  apiCall(req, res);
+});
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`listening on ${process.env.PORT}`);
+});
