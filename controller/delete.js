27c86303ba1fac3d8512cfc800d33635e5615e5c
hanschen666 <15814841133@163.com>
2021-12-27 23:02:23 +0800

    new

--- controller/delete.js
+++ controller/delete.js
@@ -3,7 +3,13 @@ const handleDelete = (req, res, db) => {
   db("users")
     .where("id", id)
     .del()
-    .then(() => res.json("success"))
+    .then(() => res.json("success delete (users)"))
+    .catch((err) => console.log("cannot delete"));
+
+  db("login")
+    .where("id", id)
+    .del()
+    .then(() => res.json("success delete (login)"))
     .catch((err) => console.log("cannot delete"));
 };
 
